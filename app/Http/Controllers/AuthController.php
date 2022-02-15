38e28dabda0157fbd281fef05d0152b1d5e2d033
Abderrahmen Brinis <72577453+dahmenbrinis@users.noreply.github.com>
2022-02-15 14:47:55 +0100

    created the authentication Controller

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+//            'device_name' => 'required',
+        ]);
+
+
+
+        if (!Auth::attempt($credentials)) {
+            return response()->json(['message'=>'Credentials not match'],401);
+        }
+
+        return response()->json(['token'=>auth()->user()->createToken('API Token')->plainTextToken]);
+    }
+
+    // this method signs out users by removing tokens
+    public function signout()
+    {
+        auth()->user()->tokens()->delete();
+
+        return [
+            'message' => 'Tokens Revoked'
+        ];
+    }
+
+
+    public function register(Request $request)
+    {
+        $attr = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|unique:users,email',
+            'password' => 'required|string|min:6|confirmed'
+        ]);
+
+        $user = User::create([
+            'name' => $attr['name'],
+            'password' => bcrypt($attr['password']),
+            'email' => $attr['email']
+        ]);
+
+        return $this->success([
+            'token' => $user->createToken('tokens')->plainTextToken
+        ]);
+    }
+}
