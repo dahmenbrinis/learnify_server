e8fa795f4be2522e927138d7aba7998b5fe23805
Abderrahmen Brinis <72577453+dahmenbrinis@users.noreply.github.com>
2022-02-15 13:41:14 +0100

    second commit

--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::get('/test',function(){
-	return 'hello world';
+	return 'hello from git';
 });
