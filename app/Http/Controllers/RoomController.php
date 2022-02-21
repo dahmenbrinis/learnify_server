53011dada9b8217a2fdeaac2493201f252034d5a
Abderrahmen Brinis <72577453+dahmenbrinis@users.noreply.github.com>
2022-02-21 02:36:37 +0100

    room indexing query optimization

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -28,9 +28,10 @@ class RoomController extends Controller
             ->orWhere('visibility','=',1)->count();
         $myRooms =Auth::user()->rooms->pluck('id');
 //$y = Room::query()->whereNotIn('id',$myRooms)->where('visibility','=',1)->orWhereIn('id',$myRooms)->count();
-        return Room::query()->whereNotIn('id',$myRooms)
-            ->where('visibility','=',1)
-            ->orWhereIn('id',$myRooms)
+        return Room::query()
+            ->WhereIn('id',$myRooms)
+            ->orWhere('visibility','=',1)
+            ->whereNotIn('id',$myRooms)
             ->orderByDesc('id')
             ->paginate(12)
             ->items();
