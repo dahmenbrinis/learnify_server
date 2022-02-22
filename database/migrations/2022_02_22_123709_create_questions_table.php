ac0b98f631bd2ae87a4605db27254ee35e1e8b4e
Abderrahmen Brinis <72577453+dahmenbrinis@users.noreply.github.com>
2022-02-22 13:49:17 +0100

    added the question model with its all policy, controller, migration etc.

---
+++ database/migrations/2022_02_22_123709_create_questions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->index('room_id');
+            $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->foreignId('room_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
